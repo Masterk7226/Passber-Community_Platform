7dcc8ecc17081fcbc42f46783b90ee7108ca5907
Philips-Ho <160144192@stu.vtc.edu.hk>
2018-04-15 23:21:53 +0800

    Geomapping update

--- Community Platform/root/js/MAPJSON.js
+++ Community Platform/root/js/MAPJSON.js
@@ -1,34 +1,12 @@
 function getMAPJSON(callback){
  var dataArray = [];
- var ref = firebase.database().ref("Platform/ABCClub/EventForm");
+ var url = new URL(location.href);
+ var communityID = url.searchParams.get("communityID");
+ var ref = firebase.database().ref("Community/" + communityID);
  ref.on("value", function(data) { // In the Form Level
   data.forEach(function(childData) { // In the Key Level
-  var feeString = childData.val().Fee;
-  var feeArray = feeString.split("Members: ");
-  var fees = [];
-  for(var i=1; i<feeArray.length; i++){
-   var fee = feeArray[i].split(" Fee: ");
-	  var obj = {};
-	  obj[fee[0]] = fee[1];
-   fees.push(obj);
-  }
-  var data = new DataSet({
-   "start":childData.val().DoS,
-   "end":childData.val().DoE,
-   "address":childData.val().Address,
-   "name":childData.val().Name,
-   "stat":"upcoming",
-   "imgsrc":childData.val().Logo,
-   "content":childData.val().Content,
-   "deadline":"",
-   "longtitue":childData.val().Long,
-   "latitue":childData.val().Lat,
-   "page":"about:blank",
-   "fee":fees
-  });
-  dataArray.push(data);
+   callback(childData);
   });
  });
- callback(dataArray);
 }
 
