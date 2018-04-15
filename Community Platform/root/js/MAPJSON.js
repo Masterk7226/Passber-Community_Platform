69270f86c691e9b3c6692d3de689b33ebeb8b59c
Philips-Ho <160144192@stu.vtc.edu.hk>
2018-04-16 03:01:59 +0800

    get JSON

--- Community Platform/root/js/MAPJSON.js
+++ Community Platform/root/js/MAPJSON.js
@@ -2,9 +2,11 @@ function getMAPJSON(callback){
  var dataArray = [];
  var url = new URL(location.href);
  var communityID = url.searchParams.get("communityID");
- var ref = firebase.database().ref("Community/" + communityID +"/EventSet");
+ var communityRef = firebase.database().ref("Community/" + communityID);
+ var ref = communityRef.child("/EventSet");
  ref.on("value", function(data) { // In the Form Level
   data.forEach(function(childData) { // In the Key Level
+  console.log(childData);
    callback(childData);
   });
  });
