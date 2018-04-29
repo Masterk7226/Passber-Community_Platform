dceaec6d74aba1e5d91daa2ab0b2265c2d75cc64
Philips-Ho <160144192@stu.vtc.edu.hk>
2018-04-29 21:10:41 +0800

    all events

--- Community Platform/root/js/MAPJSON.js
+++ Community Platform/root/js/MAPJSON.js
@@ -4,14 +4,31 @@ function getMAPJSON(callback){
  // Get Community ID from the url
  var url = new URL(location.href);
  var communityID = url.searchParams.get("communityID");
- var communityRef = firebase.database().ref("Community/" + communityID);
- var ref = communityRef.child("/EventSet");
- // Use callback to handle the data
- ref.on("value", function(data) { // In the Form Level
-  data.forEach(function(childData) { // In the Key Level
-  console.log(childData);
-   callback(childData);
+ if(communityID!=null){
+  var communityRef = firebase.database().ref("Community/" + communityID);
+  var ref = communityRef.child("/EventSet");
+  // Use callback to handle the data
+  ref.on("value", function(data) { // In the Form Level
+   data.forEach(function(childData) { // In the Key Level
+    callback(childData);
+   });
   });
- });
+ } else {
+  var allRef = firebase.database().ref("Community");
+  allRef.on("value", function(dt){
+   var communities = dt.val();
+   for(var key = [] in communities){
+    var communityID = key;
+    var communityRef = firebase.database().ref("Community/" + communityID);
+    var ref = communityRef.child("/EventSet");
+    // Use callback to handle the data
+    ref.on("value", function(data) { // In the Form Level
+     data.forEach(function(childData) { // In the Key Level
+      callback(childData);
+     });
+    });
+   }
+  });
+ }
 }
 
