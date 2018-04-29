2286300747ae4f218d9952a7068e03c2ea1d69f5
Philips-Ho <160144192@stu.vtc.edu.hk>
2018-04-29 21:25:30 +0800

    prevent wrong data

--- Community Platform/root/js/newData.js
+++ Community Platform/root/js/newData.js
@@ -1,17 +1,31 @@
 function DataSet(obj){
  // Collect Event Data
- this.id = obj.id;
- this.date_start = obj.start;
- this.date_end = obj.end;
- this.evt_address = obj.address;
- this.evt_name = obj.name;
- this.evt_stat = obj.stat;
- this.imgsrc = obj.imgsrc;
- this.evt_content = obj.content;
- this.evt_deadline = obj.deadline;
- this.longtitue = obj.longtitue;
- this.latitue = obj.latitue;
+ this.id = (obj.id == null) ? obj.id : '1';
+ this.date_start = (obj.start == null) ? obj.start : getToday();
+ this.date_end = (obj.end == null) ? obj.end : getToday();
+ this.evt_address = (obj.address == null) obj.address : "Hong Kong";
+ this.evt_name = (obj.name == null) ? obj.name : 'Unknown';
+ this.evt_stat = (obj.stat == null) ? obj.stat : 'Unknown';
+ this.imgsrc = (obj.imgsrc == null) ? obj.imgsrc : 'http://www.john-james-andersen.com/wp-content/uploads/nullimage1.gif';
+ this.evt_content = (obj.content == null) ? obj.content : '';
+ this.evt_deadline = (obj.deadline == null) ? obj.deadline : getToday();
+ this.longtitue = (obj.longtitue == null) ? obj.lontitue : '114.1';
+ this.latitue = (obj.latitue == null) ? obj.latitue : '22.30';
  this.evt_fee = obj.fee;
+ 
+ function getToday(){
+  var date = new Date();
+  var year = date.getFullYear().toString();
+  var month = date.getMonth().toString();
+  var date = date.getDate().toString();
+  if(parseInt(date) < 10){
+   date = '0' + date;
+  }
+  if(parseInt(month) < 10){
+   month = '0' + month;
+  }
+  return date + '/' + month + '/' + year;
+ }
 }
 DataSet.prototype.getFee = function(){
  var str = '';
